ce3b577a95bb47e97437c3a9e904c7bd5d727b6c
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-06-13 02:26:34 +0100

    feat(heading): render notches

--- src/components/HeadingIndicator.tsx
+++ src/components/HeadingIndicator.tsx
@@ -1,7 +1,16 @@
+// add the N, E, S, W markings
 export const HeadingIndicator = () => {
+  const points = [0, 45, 90, 135, 180, 225, 270, 315]
   return (
-    <div className="bg-red-500 size-80 rounded-full relative self-center">
-      <div className="h-8 w-1 bg-white absolute m-auto left-0 right-0" />
+    <div className="bg-black size-80 rounded-full relative self-center">
+      {points.map((point) => (
+        <div
+          style={{
+            transform: `translate(-50%, -100%) rotate(${point}deg) translateY(-128px)`
+          }}
+          className="h-8 w-1 bg-white absolute top-1/2 left-1/2 origin-bottom"
+        />
+      ))}
     </div>
   )
 }
