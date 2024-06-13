4c47b1b6dc457c3c4fc3ef74965f97275f74f3f5
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-06-13 01:40:21 +0100

    feat(heading-indicator): initial work in progress

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { WindForm } from './WindForm'
 import { Correction } from '@/types'
 import { ModeToggle } from './ModeToggle'
+import { HeadingIndicator } from './HeadingIndicator'
 
 const compassDirections: Record<number, string> = {
   360: 'North',
@@ -20,6 +21,7 @@ export default function App() {
   return (
     <div className="bg-gray-50 p-4 dark:bg-slate-900 flex flex-col grow min-h-screen">
       <ModeToggle />
+      <HeadingIndicator />
       <div>
         <ul>
           {windCorrections?.map((windCorrection) => (
---
+++ src/components/HeadingIndicator.tsx
@@ -0,0 +1,7 @@
+export const HeadingIndicator = () => {
+  return (
+    <div className="bg-red-500 size-80 rounded-full relative self-center">
+      <div className="h-8 w-1 bg-white absolute m-auto left-0 right-0" />
+    </div>
+  )
+}
