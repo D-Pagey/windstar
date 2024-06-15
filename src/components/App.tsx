dcdda93b795ad0e3fedaebbbdbf4990887c03337
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-06-15 21:47:34 +0100

    REMOVE DARK MODE

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 import { WindForm } from './WindForm'
 import { Correction } from '@/types'
-import { ModeToggle } from './ModeToggle'
 import { HeadingIndicator } from './HeadingIndicator'
-import { HeadingSvg } from './HeadingSvg'
+// import { HeadingSvg } from './HeadingSvg'
 
 export default function App() {
   const [windCorrections, setWindCorrections] = useState<Correction[]>()
 
   return (
     <div className="bg-gray-50 p-4 dark:bg-slate-900 flex flex-col grow min-h-screen">
-      <ModeToggle />
       <HeadingIndicator windCorrections={windCorrections} />
       <WindForm setWindCorrection={setWindCorrections} />
       {/* <HeadingSvg /> */}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from '@/components/App'
-import { ThemeProvider } from '@/components/ThemeProvider'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(
-  <ThemeProvider defaultTheme="dark" storageKey="windstar-ui-theme">
-    <App />
-  </ThemeProvider>
-)
+root.render(<App />)
 
 export default App
