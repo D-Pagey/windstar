b88aec60e15cf3f34186ae467c9ac4e5e0bcb381
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-06-17 22:52:42 +0100

    fix tests

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -2,17 +2,17 @@ import { getCorrectionAngleAndSpeed } from './utils'
 
 describe('getCorrectionAngleAndSpeed', () => {
   it.each`
-    windDirection | windSpeed | trueAirspeed | trueCourse | expected
-    ${270}        | ${30}     | ${120}       | ${360}     | ${{ groundSpeed: 116, correctionAngle: -14.5 }}
-    ${270}        | ${30}     | ${120}       | ${90}      | ${{ groundSpeed: 150, correctionAngle: 0 }}
-    ${270}        | ${30}     | ${120}       | ${45}      | ${{ groundSpeed: 139, correctionAngle: -10.2 }}
+    windDirection | windSpeed | trueAirSpeed | trueCourse | expected
+    ${270}        | ${30}     | ${120}       | ${360}     | ${{ groundSpeed: 116, correctionAngle: -14.5, trueCourse: 360 }}
+    ${270}        | ${30}     | ${120}       | ${90}      | ${{ groundSpeed: 150, correctionAngle: 0, trueCourse: 90 }}
+    ${270}        | ${30}     | ${120}       | ${45}      | ${{ groundSpeed: 139, correctionAngle: -10.2, trueCourse: 45 }}
   `(
     'should return correct wind angle and speed',
-    ({ windDirection, windSpeed, trueAirspeed, trueCourse, expected }) => {
+    ({ windDirection, windSpeed, trueAirSpeed, trueCourse, expected }) => {
       const result = getCorrectionAngleAndSpeed({
         windDirection,
         windSpeed,
-        trueAirspeed,
+        trueAirSpeed,
         trueCourse
       })
       expect(result).toStrictEqual(expected)
